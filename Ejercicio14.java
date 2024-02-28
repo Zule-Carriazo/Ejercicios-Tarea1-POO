a956468af2ba8887bf1cea6aba34022a936e6c66
Zule <71475758+Zule-Carriazo@users.noreply.github.com>
2024-02-28 10:38:27 -0500

    Ejercicio14

---
+++ Ejercicio14.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.ejercicio14;
+public class Ejercicio14 {
+
+    public static void main(String[] args) {
+        double numero, cuadrado, cubo;
+        numero=3;
+        cuadrado=Math.pow(numero,2);
+        cubo=Math.pow(numero,3);
+        System.out.println("El numero es: " + numero);
+        System.out.println("Su cuadrado es: " + cuadrado);
+        System.out.println("Y su cubo es: " + cubo);
+    }
+}
+
+
+
+
+
