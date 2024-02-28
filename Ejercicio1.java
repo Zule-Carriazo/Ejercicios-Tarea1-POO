fabca0447f68d0e3968006f930dd488eac071cc3
Zule <71475758+Zule-Carriazo@users.noreply.github.com>
2024-02-28 10:34:43 -0500

    Ejercicio4

---
+++ Ejercicio1.java
@@ -0,0 +1,19 @@
+package com.mycompany.ejercicio1;
+
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        int edjuan= 9;
+        int edalber, edana, edmama;
+        edalber= 2*edjuan/3;
+        edana=4*edjuan/3;
+        edmama=edjuan+ edalber + edana;
+       
+        System.out.println("Las edades de cada uno son: ");
+        System.out.println("Juan:" +edjuan);
+        System.out.println("Alberto "+edalber);
+        System.out.println("Ana : " +edana);
+        System.out.println("Mama: " +edmama);
+    }
+}
